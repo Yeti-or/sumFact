875bad4b3c3436fece72ae2cb2f393d2c4c80897
Yeti <Yeti.or@gmail.com>
2013-08-18 01:41:11 +0400

    Add function to count sum of factorial numbers.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
 console.log('\'Allo \'Allo!');
+
+var getFactorial = function(num){
+    if(num === 0) return 1;
+    return getFactorial(num-1)*num;
+};
+
+var checkingFn = function(from,to){
+    var result = [];
+    for(var i=from;i<=to;i++){
+        var numbers = (i.toString());
+        var sum = 0; 
+        for(var j=numbers.length;j--;){
+            sum += getFactorial(parseInt(numbers[j],10));
+        }
+        console.log(i+'_'+sum);
+        if(i === sum){ result.push(i); }
+    }
+    console.log('ok');
+    console.log(result);
+};
+
+checkingFn(0,1000);
+console.log(getFactorial(5));
