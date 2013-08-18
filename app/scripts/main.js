9b9641299278f42e42149fb8333cf496b3855e87
Yeti <Yeti.or@gmail.com>
2013-08-18 04:50:30 +0400

    Add upper bound and improve factorialFn.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,24 @@
+'use strict';
 
 var getFactorial = function(num){
     if(num === 0) return 1;
-    return getFactorial(num-1)*num;
+    if(num === 1) return 1;
+    if(num === 2) return 2;
+    if(num === 3) return 6;
+    if(num === 4) return 24;
+    if(num === 5) return 120;
+    if(num === 6) return 720;
+    if(num === 7) return 5040;
+    if(num === 8) return 40320;
+    if(num === 9) return 362880;
+    //return getFactorial(num-1)*num;
 };
 
 var checkingFn = function(from,to){
     var result = [];
     for(var i=from;i<=to;i++){
         var numbers = (i.toString());
-        var sum = 0; 
+        var sum = 0;
         for(var j=numbers.length;j--;){
             sum += getFactorial(parseInt(numbers[j],10));
         }
@@ -21,7 +31,7 @@ var checkingFn = function(from,to){
 
 var startNode;
 var endNode;
-var resulrNode;
+var resultNode;
 
 var go  = function(){
     event.preventDefault();
@@ -38,5 +48,7 @@ window.onload = function(){
     startNode = document.getElementById('start');
     endNode = document.getElementById('end');
     resultNode = document.getElementById('result');
+    startNode.value = 0;
+    endNode.value = 2540160;//7*9!
     go();
 };
